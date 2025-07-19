4d614e4faaf2085da80321c5358c9eadffce9f4f
José Queiroz <jqueiroz.av@gmail.com>
2025-07-19 19:35:29 -0300

    Added flyway dependency and first migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Boa Viagem, Recife/PE', 'José', 'Male', 'Queiroz'),
+	(2, 'Boa Viagem, Recife/PE', 'Ana', 'Female', 'Souza'),
+	(3, 'Piedade, Jaboatão/PE', 'Vania', 'Female', 'Albuquerque');
