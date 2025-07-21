917519b9ced97678a9d0f89611d65f94f547ff5e
José Queiroz <jqueiroz.av@gmail.com>
2025-07-21 09:05:19 -0300

    Content negotiation with Header Param

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/config/WebConfig.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/config/WebConfig.java
@@ -14,9 +14,19 @@ public class WebConfig implements WebMvcConfigurer {
         // Via EXTENSION _.xml or _.json DEPRECATED on Spring Boot 2.6
 
         // Via QUERY PARAM _?mediaType=json
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        /*
+         * configurer.favorParameter(true)
+         * .parameterName("mediaType")
+         * .ignoreAcceptHeader(true)
+         * .useRegisteredExtensionsOnly(false)
+         * .defaultContentType(MediaType.APPLICATION_JSON)
+         * .mediaType("json", MediaType.APPLICATION_JSON)
+         * .mediaType("xml", MediaType.APPLICATION_XML);
+         */
+
+        // Via HEADER PARAM _?mediaType=json
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
