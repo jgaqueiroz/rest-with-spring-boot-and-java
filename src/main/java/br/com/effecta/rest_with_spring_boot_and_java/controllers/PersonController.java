782bbfed0812e10e01d96b359efbb52ee654b71d
José Queiroz <jqueiroz.av@gmail.com>
2025-07-21 21:02:17 -0300

    YAML serialization

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -24,25 +24,52 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @GetMapping(
+        produces = { 
+            MediaType.APPLICATION_JSON_VALUE, 
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE 
+        })
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @GetMapping(value = "/{id}", 
+        produces = { 
+            MediaType.APPLICATION_JSON_VALUE, 
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE 
+        })
     public PersonDTO findById(@PathVariable Long id) {
         return service.findById(id);
     }
 
-    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, produces = {
-            MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @PostMapping(
+        consumes = { 
+            MediaType.APPLICATION_JSON_VALUE, 
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE 
+        }, 
+        produces = {
+            MediaType.APPLICATION_JSON_VALUE, 
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE 
+        })
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
-    @PutMapping(value = "/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE })
+    @PutMapping(value = "/{id}", 
+        consumes = { 
+            MediaType.APPLICATION_JSON_VALUE, 
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE 
+        }, 
+        produces = { 
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE, 
+            MediaType.APPLICATION_YAML_VALUE 
+        })
     public PersonDTO update(@PathVariable Long id, @RequestBody PersonDTO person) {
         return service.update(id, person);
     }
