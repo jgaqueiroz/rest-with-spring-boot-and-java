2798e098a639a32f5f0268b7889eec3a82e2983c
José Queiroz <jqueiroz.av@gmail.com>
2025-07-26 23:44:36 -0300

    Created BookService

---
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/services/BookService.java
@@ -0,0 +1,79 @@
+package br.com.effecta.rest_with_spring_boot_and_java.services;
+
+import static br.com.effecta.rest_with_spring_boot_and_java.mapper.ObjectMapper.parseListObjects;
+import static br.com.effecta.rest_with_spring_boot_and_java.mapper.ObjectMapper.parseObject;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.effecta.rest_with_spring_boot_and_java.data.dto.BookDTO;
+import br.com.effecta.rest_with_spring_boot_and_java.exceptions.RequiredObjectIsNullException;
+import br.com.effecta.rest_with_spring_boot_and_java.exceptions.ResourceNotFoundException;
+import br.com.effecta.rest_with_spring_boot_and_java.model.Book;
+import br.com.effecta.rest_with_spring_boot_and_java.repositories.BookRepository;
+
+@Service
+public class BookService {
+
+    private Logger logger = LoggerFactory.getLogger(BookService.class.getName());
+
+    @Autowired
+    private BookRepository repository;
+
+    public List<BookDTO> findAll() {
+        logger.info("Finding all Books!");
+
+        var books = parseListObjects(repository.findAll(), BookDTO.class);
+        return books;
+    }
+
+    public BookDTO findById(Long id) {
+        logger.info("Finding one Book!");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with ID: " + id));
+        var dto = parseObject(entity, BookDTO.class);
+        return dto;
+    }
+
+    public BookDTO create(BookDTO book) {
+        if (book == null) throw new RequiredObjectIsNullException();
+
+        logger.info("Creating one Book!");
+
+        var entity = parseObject(book, Book.class);
+        var dto = parseObject(repository.save(entity), BookDTO.class);
+        return dto;
+    }
+
+    public BookDTO update(Long id, BookDTO book) {
+        if (book == null) throw new RequiredObjectIsNullException();
+        
+        logger.info("Updating one Book!");
+
+        Book entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with ID: " + id));
+        if (book.getAuthor() != null)
+            entity.setAuthor(book.getAuthor());
+        if (book.getLaunchDate() != null)
+            entity.setLaunchDate(book.getLaunchDate());
+        if (book.getPrice() != null)
+            entity.setPrice(book.getPrice());
+        if (book.getTitle() != null)
+            entity.setTitle(book.getTitle());
+        var dto = parseObject(repository.save(entity), BookDTO.class);
+        return dto;
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one Book!");
+
+        findById(id);
+        repository.deleteById(id);
+    }
+    
+}
