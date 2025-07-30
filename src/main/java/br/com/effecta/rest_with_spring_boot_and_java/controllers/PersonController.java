58ade4c71e292ec2e1ffa55612780671f4064315
José Queiroz <jqueiroz.av@gmail.com>
2025-07-30 19:01:56 -0300

    Implementing CORS on Controllers

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.effecta.rest_with_spring_boot_and_java.data.dto.PersonDTO;
 import br.com.effecta.rest_with_spring_boot_and_java.services.PersonService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -38,6 +40,7 @@ public class PersonController implements PersonControllerDocs {
         return service.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @Override
     @GetMapping(value = "/{id}", 
         produces = { 
@@ -49,6 +52,7 @@ public class PersonController implements PersonControllerDocs {
         return service.findById(id);
     }
 
+    @CrossOrigin(origins = {"http://localhost:8080", "https://effecta.com.br"})
     @Override
     @PostMapping(
         consumes = { 
