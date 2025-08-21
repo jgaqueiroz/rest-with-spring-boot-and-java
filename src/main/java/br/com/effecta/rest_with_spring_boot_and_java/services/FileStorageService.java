5fce7c7ae33d5260b8590a0a3ee5bed4beb21625
José Queiroz <jqueiroz.av@gmail.com>
2025-08-21 09:11:45 -0300

    Created FileStorageService

---
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/services/FileStorageService.java
@@ -0,0 +1,50 @@
+package br.com.effecta.rest_with_spring_boot_and_java.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.effecta.rest_with_spring_boot_and_java.config.FileStorageConfig;
+import br.com.effecta.rest_with_spring_boot_and_java.exceptions.FileStorageException;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().toAbsolutePath().normalize();
+        
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filaname contains an invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e)
+        }
+    }
+    
+}
