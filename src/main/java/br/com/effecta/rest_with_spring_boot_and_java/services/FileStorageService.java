e9ad5b202d1c772e074d417bfe48e8c3d9e9134c
José Queiroz <jqueiroz.av@gmail.com>
2025-08-22 08:55:05 -0300

    Created service to download files

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/services/FileStorageService.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/services/FileStorageService.java
@@ -8,11 +8,14 @@ import java.nio.file.StandardCopyOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.effecta.rest_with_spring_boot_and_java.config.FileStorageConfig;
+import br.com.effecta.rest_with_spring_boot_and_java.exceptions.FileNotFoundException;
 import br.com.effecta.rest_with_spring_boot_and_java.exceptions.FileStorageException;
 
 @Service
@@ -53,6 +56,22 @@ public class FileStorageService {
         } catch (Exception e) {
             logger.error("Could not store file " + fileName + ". Please try again!");
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                logger.error("File not found " + fileName);
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (Exception e) {
+            logger.error("File not found " + fileName);
+            throw new FileNotFoundException("File not found " + fileName, e);
         }
     }
     
