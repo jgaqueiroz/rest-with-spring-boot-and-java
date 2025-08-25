be1d506d3a3369df38c0d68382795b6932e3f187
José Queiroz <jqueiroz.av@gmail.com>
2025-08-25 19:27:35 -0300

    Implementing CSV importer

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/file/importer/impl/CsvImporter.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/file/importer/impl/CsvImporter.java
@@ -1,17 +1,45 @@
 package br.com.effecta.rest_with_spring_boot_and_java.file.importer.impl;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Component;
 
 import br.com.effecta.rest_with_spring_boot_and_java.data.dto.PersonDTO;
 import br.com.effecta.rest_with_spring_boot_and_java.file.importer.contract.FileImporter;
 
+@Component
 public class CsvImporter implements FileImporter {
 
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'importFile'");
+        CSVFormat format = CSVFormat.Builder.create()
+            .setHeader()
+            .setSkipHeaderRecord(true)
+            .setIgnoreEmptyLines(true)
+            .setTrim(true)
+            .get();
+
+        Iterable<CSVRecord> records =  format.parse(new InputStreamReader(inputStream));
+        return parseRecordsToPersonDTOs(records);
+    }
+
+    private List<PersonDTO> parseRecordsToPersonDTOs(Iterable<CSVRecord> records) {
+        List<PersonDTO> people = new ArrayList<>();
+        for (CSVRecord record : records) {
+            PersonDTO person = new PersonDTO();
+            person.setFirstName(record.get("first_name"));
+            person.setLastName(record.get("last_name"));
+            person.setAddress(record.get("address"));
+            person.setGender(record.get("gender"));
+            person.setEnabled(true);
+            people.add(person);
+        }
+        return people;
     }
     
 }
