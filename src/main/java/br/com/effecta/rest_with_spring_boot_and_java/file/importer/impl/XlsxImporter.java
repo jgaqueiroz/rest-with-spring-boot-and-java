77daf2d480efd28dc06cc016a234ffc65cf4cbb2
José Queiroz <jqueiroz.av@gmail.com>
2025-08-26 20:51:03 -0300

    Implementing XLSX importer

--- src/main/java/br/com/effecta/rest_with_spring_boot_and_java/file/importer/impl/XlsxImporter.java
+++ src/main/java/br/com/effecta/rest_with_spring_boot_and_java/file/importer/impl/XlsxImporter.java
@@ -1,17 +1,58 @@
 package br.com.effecta.rest_with_spring_boot_and_java.file.importer.impl;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
 
 import br.com.effecta.rest_with_spring_boot_and_java.data.dto.PersonDTO;
 import br.com.effecta.rest_with_spring_boot_and_java.file.importer.contract.FileImporter;
 
+@Component
 public class XlsxImporter implements FileImporter {
 
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'importFile'");
+        
+        try (XSSFWorkbook workbook = new XSSFWorkbook(inputStream)) {
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            if (rowIterator.hasNext()) rowIterator.next();
+
+            return parseRowsToPersonDTO(rowIterator);
+        }
+    }
+
+    private List<PersonDTO> parseRowsToPersonDTO(Iterator<Row> rowIterator) {
+        List<PersonDTO> people = new ArrayList<>();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            if (isRowValid(row)) {
+                people.add(parseRowToPersonDTO(row));
+            }
+        }
+        return people;
+    }
+
+    private PersonDTO parseRowToPersonDTO(Row row) {
+        PersonDTO person = new PersonDTO();
+        person.setFirstName(row.getCell(0).getStringCellValue());
+        person.setLastName(row.getCell(1).getStringCellValue());
+        person.setAddress(row.getCell(2).getStringCellValue());
+        person.setGender(row.getCell(3).getStringCellValue());
+        person.setEnabled(true);
+        return person;
+    }
+
+    private boolean isRowValid(Row row) {
+        return row.getCell(0) != null && row.getCell(0).getCellType() != CellType.BLANK;
     }
     
 }
